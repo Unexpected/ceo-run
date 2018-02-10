cd3668debc90690e7eb832a6a160d5d6e662c89a
Francis Bellanger <francis.bellanger@cgi.com>
2018-02-10 22:28:45 +0100

    implement gravity and test collision

--- ceo-run.asm
+++ ceo-run.asm
@@ -18,27 +18,38 @@ buttons1   .rs 1  ; player 1 gamepad buttons, one bit per button
 buttons2   .rs 1  ; player 2 gamepad buttons, one bit per button
 
 ; gameplay variables
-playerPosX .rs 1
-playerPosY .rs 1
+; player state - see PLAYER_STATE_XXX constants
+playerState .rs 1
+
+; number of frames with button A pressed for jump
+; player cannont jump more than MAX_JUMP_TIME
+playerJumpTime .rs 1
+
+playerPosX 	.rs 1
+playerPosY 	.rs 1
 playerSpeedX	.rs 1
 playerSpeedY	.rs 1
 
 
+playerSpriteHi	.rs 1
+playerSpriteLo	.rs 1	
+
+; Constants
+MAX_SPEED = $03 			; max speed for player
+JUMP_SPEED = $04
+MAX_JUMP_TIME = $0A		; max number of frames to jump  
+
+PLAYER_STATE_RUNNING = $00
+PLAYER_STATE_FALLING = $01
+PLAYER_STATE_JUMPING = $02
+
+PLAYER_SPRITES = $04	; number of sprites for player
+SPRITE_POS_X = $03		; sprite is Y NUMBER ATTR X 
+SPRITE_POS_Y = $00		; sprite is Y NUMBER ATTR X 
+
 
   .bank 0
   .org $C000 
-
-subroutine: 
-  LDA $0203       ; load sprite X position
-  SEC             ; make sure carry flag is set
-  SBC #$02        ; A = A - 1
-  STA $0203       ; save sprite X position
-  STA $020B
-  ADC #$07
-  STA $0207
-  STA $020F
-  RTS  
-  
   
 RESET:
   SEI          ; disable IRQs
@@ -99,7 +110,7 @@ LoadPalettesLoop:
 LoadSprites:
   LDX #$00              ; start at 0
 LoadSpritesLoop:
-  LDA sprites, x        ; load data from address (sprites +  x)
+  LDA playerSprites, x  ; load data from address (sprites +  x)
   STA $0200, x          ; store into RAM address ($0200 + x)
   INX                   ; X = X + 1
   CPX #$10              ; Compare X to hex $10, decimal 16
@@ -190,15 +201,27 @@ InitGame:
   ; initial player pos
   LDA #$40		
   STA playerPosX
-  LDA #$50
-  STA playerPosY
+  LDA #$10
+  STA playerPosY	; near top of the screen
 
+  ; initial player state
+  LDA #PLAYER_STATE_FALLING
+  STA playerState
+  LDA $00
+  STA playerJumpTime
+  
   ; initial player speed
   LDA #$00
   STA playerSpeedX
   STA playerSpeedY
-              
-              
+  
+  LDA #$02
+  STA playerSpriteHi
+  LDA #$00
+  STA playerSpriteLo
+  
+  
+  
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
   STA $2000
 
@@ -211,10 +234,15 @@ Forever:
  
 
 NMI:
-  INC scroll       ; add one to our scroll variable each frame
+  LDA scroll
+  ADC playerSpeedX
+  ;INC scroll       ; add one to our scroll variable each frame
 NTSwapCheck:
-  LDA scroll       ; check if the scroll just wrapped from 255 to 0
-  BNE NTSwapCheckDone
+  ;LDA scroll       ; check if the scroll just wrapped from 255 to 0
+  ;BNE NTSwapCheckDone
+  CMP scroll
+  STA scroll
+  BCS NTSwapCheckDone
   
 NTSwap:
   LDA nametable    ; load current nametable number (0 or 1)
@@ -251,6 +279,12 @@ NTSwapCheckDone:
   ; run normal game engine code here
   ; reading from controllers, etc
 
+;****************************************************************************
+;****************************                  ******************************
+;**************************** MAIN GAME ENGINE ******************************
+;****************************                  ******************************
+;****************************************************************************
+  
   JSR ReadController1  ;;get the current button data for player 1
   JSR ReadController2  ;;get the current button data for player 2
 
@@ -272,8 +306,13 @@ GameEngineDone:
   
   RTI              ; return from interrupt
  
-;;;;;;;;;;;;;;  
-
+;****************************************************************************
+;****************************                  ******************************
+;****************************    SUBROUTINES   ******************************
+;****************************                  ******************************
+;****************************************************************************
+  
+; Read controller 1 state
 ReadController1:
   LDA #$01
   STA $4016
@@ -288,6 +327,7 @@ ReadController1Loop:
   BNE ReadController1Loop
   RTS
   
+; Read controller 2 state
 ReadController2:
   LDA #$01
   STA $4016
@@ -302,8 +342,124 @@ ReadController2Loop:
   BNE ReadController2Loop
   RTS  
 
+; Gameplay instructions
 UpdateGameState:
+  JSR HandleInput
+  JSR ApplyPhysics
+  JSR CheckCollision
+  JSR UpdatePlayer
+  
   RTS
+
+; Update vertical speed based on controller input
+HandleInput: 
+  LDA buttons1
+  AND #%10000000
+  BEQ HandleNoJump		; player doesn't press A
+    
+  LDA playerState
+  CMP #PLAYER_STATE_FALLING
+  BEQ HandleInputDone		; player currently falling
+
+  LDA #PLAYER_STATE_JUMPING
+  STA playerState
+  INC playerJumpTime
+  JMP HandleInputDone
+  
+HandleNoJump: 
+  LDA playerState
+  CMP #PLAYER_STATE_JUMPING
+  BNE HandleNoJumpDone
+  LDA #PLAYER_STATE_FALLING
+  STA playerState  
+HandleNoJumpDone: 
+HandleInputDone: 
+  RTS
+
+  
+; Apply "physics" (gravity and acceleration)
+ApplyPhysics: 
+  LDA playerState
+  CMP #PLAYER_STATE_JUMPING		; player is jumping
+  BNE JumpDone
+  LDX playerJumpTime
+  CPX #MAX_JUMP_TIME
+  BNE MaxJumpTimeReachedDone	; if player has reached max jump time, stop jumping
+MaxJumpTimeReached: 
+  LDX #PLAYER_STATE_FALLING
+  STX playerState
+MaxJumpTimeReachedDone: 
+  LDA playerState
+  CMP #PLAYER_STATE_JUMPING		; player is jumping
+  BNE JumpDone
+Jump: 
+  LDX #JUMP_SPEED
+  STX playerSpeedY
+JumpDone: 
+  CMP #PLAYER_STATE_FALLING		; player is falling
+  BNE FallDone
+Fall: 
+  LDX playerSpeedY
+  CPX #$FD						; check if speedY if already falling at max speed
+  BEQ FallDone
+  DEC playerSpeedY
+FallDone: 
+  
+ApplyPhysicsDone: 
+  RTS
+
+; Check collisions with platforms
+CheckCollision: 
+  LDA playerState
+  CMP #PLAYER_STATE_FALLING		; player is jumping
+  BNE CheckGroundCollisionDone
+CheckGroundCollision:
+  LDA playerPosY
+  CMP #$D0
+  BNE CheckCollisionDone
+  LDX #$00
+  STX playerSpeedY
+  STX playerJumpTime
+  LDX #PLAYER_STATE_RUNNING
+  STX playerState
+CheckGroundCollisionDone:
+CheckCollisionDone: 
+  RTS
+
+
+; Update player pos based on speed variables
+UpdatePlayer: 
+  LDX playerSpeedY
+  CPX #$00
+  BCC GoingUp
+GoingDown: 
+  INC playerPosY
+  INX
+  BNE GoingDown
+  JMP GoingUpDone
+GoindDownDone: 
+GoingUp: 
+  DEC playerPosY
+  DEX
+  BNE GoingUp
+GoingUpDone: 
+  LDA playerPosY
+  STA $0200
+  LDA playerPosX
+  STA $0203
+UpdatePlayerDone: 
+  RTS
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; unused ?  
+AccelerateX:
+  LDA playerSpeedX
+  CMP #MAX_SPEED
+  BCS AccelerateXDone
+  INC playerSpeedX
+AccelerateXDone: 
+  RTS
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; unused ?  
 
   
   .bank 1
@@ -312,7 +468,7 @@ palette:
   .db $22,$29,$1A,$0F,  $22,$36,$17,$0F,  $22,$30,$21,$0F,  $22,$27,$17,$0F   ;;background palette
   .db $22,$16,$36,$12,  $22,$02,$38,$3C,  $22,$1C,$15,$14,  $22,$02,$38,$3C   ;;sprite palette
 
-sprites:
+playerSprites:
      ;vert tile attr horiz
   .db $80, $32, $00, $80   ;sprite 0
   .db $80, $33, $00, $88   ;sprite 1
