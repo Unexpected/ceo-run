0fecbeb22158b1ccb08f0b5016e0bd24a939d450
Francis Bellanger <francis.bellanger@cgi.com>
2018-02-17 23:19:28 +0100

    init platform drawing

--- ceo-run.asm
+++ ceo-run.asm
@@ -9,7 +9,14 @@
 
 ; scrolling variables
 scroll     .rs 1  ; horizontal scroll count
-nametable  .rs 1  ; current nametable for swapping
+nametable  .rs 1  ; which nametable to use, 0 or 1
+columnLow  .rs 1  ; low byte of new column address
+columnHigh .rs 1  ; high byte of new column address
+sourceLow  .rs 1  ; source for column data
+sourceHigh .rs 1
+columnNumber .rs 1  ; which column of level data to draw
+
+
 
 ; controllers
 ; button: A B select start up down left right
@@ -31,10 +38,14 @@ playerPosY 	.rs 1		; bot left pixel
 playerSpeedX	.rs 1
 playerSpeedY	.rs 1
 
+platformPosX	.rs 1
+platformPosY	.rs 1
+platformLength	.rs 1
+
 ; Constants
 MAX_SPEED = $03 		; max speed for player
 JUMP_SPEED = $06
-FALL_SPEED = $FC		; FALL_SPEED == -4
+FALL_SPEED = $FB		; FALL_SPEED == -5
 MAX_JUMP_TIME = $10		; max number of frames to jump  
 
 PLAYER_HEIGHT = $10		; player meta-sprite height (used for collision)
@@ -212,10 +223,19 @@ InitGame:
   STA playerJumpTime
   
   ; initial player speed
-  LDA #$03
+  LDA #MAX_SPEED
   STA playerSpeedX
   LDA #$00
   STA playerSpeedY
+
+  ; Init next platform coordinates
+  LDA #$03
+  STA platformPosX
+  LDA #$DD
+  STA platformPosY
+  LDA #$0C
+  STA platformLength
+
   
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
   STA $2000
@@ -249,6 +269,27 @@ NTSwap:
   STA nametable    ; so if nametable was 0, now 1
                    ;    if nametable was 1, now 0
 NTSwapCheckDone:
+
+NewAttribCheck:
+  LDA scroll
+  AND #%00011111            ; check for multiple of 32
+  BNE NewAttribCheckDone    ; if low 5 bits = 0, time to write new attribute bytes
+  jsr DrawNewAttributes
+NewAttribCheckDone:
+
+
+NewColumnCheck:
+  LDA scroll
+  AND #%00000111            ; throw away higher bits to check for multiple of 8
+  BNE NewColumnCheckDone    ; done if lower bits != 0
+  JSR DrawNewColumn         ; if lower bits = 0, time for new column
+  
+  lda columnNumber
+  clc
+  adc #$01             ; go to next column
+  and #%01111111       ; only 128 columns of data, throw away top bit to wrap
+  sta columnNumber
+NewColumnCheckDone:
 
   LDA #$00
   STA $2003       
@@ -451,6 +492,153 @@ GoingUpDone:
 UpdatePlayerDone: 
   RTS
 
+;****************************************************************************
+;****************************                  ******************************
+;**************************** GRAPHIC ENGINE   ******************************
+;****************************                  ******************************
+;****************************************************************************
+    
+DrawNewColumn:
+  LDA scroll       ; calculate new column address using scroll register
+  LSR A
+  LSR A
+  LSR A            ; shift right 3 times = divide by 8
+  STA columnLow    ; $00 to $1F, screen is 32 tiles wide
+
+  LDA nametable     ; calculate new column address using current nametable
+  EOR #$01          ; invert low bit, A = $00 or $01
+  ASL A             ; shift up, A = $00 or $02
+  ASL A             ; $00 or $04
+  CLC
+  ADC #$20          ; add high byte of nametable base address ($2000)
+  STA columnHigh    ; now address = $20 or $24 for nametable 0 or 1
+
+;  LDA columnNumber  ; column number * 32 = column data offset
+;  ASL A
+;  ASL A
+;  ASL A
+;  ASL A
+;  ASL A             
+;  STA sourceLow
+;  LDA columnNumber
+;  LSR A
+;  LSR A
+;  LSR A
+;  STA sourceHigh
+  
+;  LDA sourceLow       ; column data start + offset = address to load column data from
+;  CLC 
+;  ADC #LOW(columnData)
+;  STA sourceLow
+;  LDA sourceHigh
+;  ADC #HIGH(columnData)
+;  STA sourceHigh
+
+DrawColumn:
+  LDA #%00000100        ; set to increment +32 mode
+  STA $2000
+  
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA columnHigh
+  STA $2006             ; write the high byte of column address
+  LDA columnLow
+  STA $2006             ; write the low byte of column address
+  LDX #$1E              ; copy 30 bytes
+  
+  LDA platformPosX		; check if we are drawing a platform
+  CMP #$00
+  BEQ DrawColumnLoop    ; draw a platform
+  
+  DEC A
+  STA platformPosX		; decrement platform distance
+DrawEmptyColumnLoop: 
+  LDA #$24
+  STA $2007	
+  DEX
+  BNE DrawEmptyColumnLoop
+  JMP DrawColumnLoopDone
+  
+DrawColumnLoop:
+  CPX platformPosY
+  BNE LoadBlock
+  LDA #$24		
+  JMP Draw
+LoadBlock: 
+  LDA #$45  	; if platform : draw blue except for platform sprite
+Draw:   
+  STA $2007					; else draw blue
+  DEX
+  BNE DrawColumnLoop
+  
+  ; last platform block ?
+  LDA platformLength
+  CMP #$00
+  BEQ PrepareNewPlatform
+  DEC A
+  STA platformLength
+  JMP DrawColumnLoopDone
+
+PrepareNewPlatform: 
+  ; Init next platform coordinates
+  LDA #$03
+  STA platformPosX
+  LDA platformPosY
+  ASL A
+  STA platformPosY
+  LDA #$0C
+  STA platformLength
+  
+DrawColumnLoopDone:
+
+  RTS
+  
+  
+  
+DrawNewAttributes:
+  LDA nametable
+  EOR #$01          ; invert low bit, A = $00 or $01
+  ASL A             ; shift up, A = $00 or $02
+  ASL A             ; $00 or $04
+  CLC
+  ADC #$23          ; add high byte of attribute base address ($23C0)
+  STA columnHigh    ; now address = $23 or $27 for nametable 0 or 1
+  
+  LDA scroll
+  LSR A
+  LSR A
+  LSR A
+  LSR A
+  LSR A
+  CLC
+  ADC #$C0
+  STA columnLow     ; attribute base + scroll / 32
+
+  LDY #$00
+  LDA $2002             ; read PPU status to reset the high/low latch
+DrawNewAttributesLoop
+  LDA columnHigh
+  STA $2006             ; write the high byte of column address
+  LDA columnLow
+  STA $2006             ; write the low byte of column address
+  LDA #$FF			    ; copy new attribute byte
+  STA $2007
+  
+  INY
+  CPY #$08              ; copy 8 attribute bytes
+  BEQ DrawNewAttributesLoopDone 
+  
+  LDA columnLow         ; next attribute byte is at address + 8
+  CLC
+  ADC #$08
+  STA columnLow
+  JMP DrawNewAttributesLoop
+DrawNewAttributesLoopDone:
+
+  RTS  
+  
+  
+  
+  
 ;;;;;;;;;;;;;;;;;;;;;;;;;; unused ?  
 AccelerateX:
   LDA playerSpeedX
